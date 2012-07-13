8c0d3d0bc25faaa9eb31d74cb140b05a6876cff0
weltenwort <github.signup@weltenwort.de>
2012-07-14 00:39:14 +0200

    Remove pyzen for testing

--- src/server/manage.py
+++ src/server/manage.py
@@ -1,16 +1,13 @@
 # -*- encoding:utf-8 -*-
 from flask.ext.script import Manager, Shell
 from flask.ext.assets import ManageAssets
-from flask.ext.zen import Test, ZenTest
 
 from madacraserver import create_app
 from server import RunSocketIOServer
 
-manager = Manager(create_app(), with_default_commands=False)
+manager = Manager(create_app, with_default_commands=False)
 manager.add_command("assets", ManageAssets())
 manager.add_command("shell", Shell())
-manager.add_command("test", Test())
-manager.add_command("zen", ZenTest())
 manager.add_command("runserver", RunSocketIOServer())
 
 
